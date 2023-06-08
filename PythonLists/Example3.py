ad72203a5cf052aac03e02726a5c5b06dbcb71a4
Shantanu Rauthan <32995240+ShantanuRauthan@users.noreply.github.com>
2023-06-08 14:34:29 +0530

    Create Example3.py

---
+++ PythonLists/Example3.py
@@ -0,0 +1,11 @@
+#Write a Python program to remove duplicates from a list.
+
+#Code (2 liner):
+a = [3,7,4,3,2,2,1]
+print(list(set(a)))
+
+
+
+#Explanation:
+#Since sets cant have duplicate values , we converted given list to a set 
+#Since we want list in answer , we converted the set back to a list
