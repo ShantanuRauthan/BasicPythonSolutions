6aae564a3f6b95de4cb982a4a0991826a5f80ab3
Shantanu Rauthan <32995240+ShantanuRauthan@users.noreply.github.com>
2023-06-08 13:42:41 +0530

    Create Example1.py

---
+++ PythonLists/Example1.py
@@ -0,0 +1,14 @@
+# Write a Python program to count the number of strings from a given list of strings. 
+# The string length is 2 or more and the first and last characters are the same.
+# Sample List : ['abc', 'xyz', 'aba', '1221']
+# Expected Result : 2
+
+#Code:
+a = ['abc', 'xyz', 'aba', '1221']
+count=0
+for i in a:
+  if len(i)>=2 and i[0]==i[-1]:
+    count+=1
+  else:
+    pass
+print(count)  
